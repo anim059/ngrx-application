99535b07ba96191af86a899aead7b00c9c64d5b5
unknown <istiaquehashem59@gmail.com>
2025-09-20 21:51:04 +0600

    angular material from 18 to 19

--- ngrx-application/src/app/app.component.ts
+++ ngrx-application/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterModule, MatIconModule],
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule, MatIconModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ngrx-application';
